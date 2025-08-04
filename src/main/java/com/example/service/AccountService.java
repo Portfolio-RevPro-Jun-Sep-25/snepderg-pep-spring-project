616729424588ef525c7a62f6a05b6908c3c144af
banoargeek <thatguymajor@protonmail.com>
2025-08-04 17:08:29 -0600

    Update Account Service

--- src/main/java/com/example/service/AccountService.java
+++ src/main/java/com/example/service/AccountService.java
@@ -15,16 +15,42 @@ import java.util.List;
 @Service
 public class AccountService {
     AccountRepository accountRepository;
+
+    private static final int PASSWORD_MINIMUM_CHARACTERS = 4;
+
     @Autowired // Constructor injection
     public AccountService(AccountRepository accountRepository) {
         this.accountRepository = accountRepository;
     }
+
     public Account registerAccount(Account account) {
-        // TODO Auto-generated method stub
-        throw new UnsupportedOperationException("Unimplemented method 'registerAccount'");
+        if (account == null) {
+            return null;
+        }
+
+        String username = account.getUsername();
+        String password = account.getPassword();
+
+        // Username must not be blank.
+        if (username.isBlank()) {
+            return null;
+        }
+
+        // Password must be at least 4 characters.
+        if (password.length() < PASSWORD_MINIMUM_CHARACTERS) {
+            return null;
+        }
+
+        // Accounts must be unique.
+        boolean accountFound = accountRepository.existsByUsernameIgnoreCase(username);
+        if (accountFound) {
+            return null;
+        }
+
+        return accountRepository.save(account);
     }
+
     public Account getAccountByUsername(String username) {
-        // TODO Auto-generated method stub
-        throw new UnsupportedOperationException("Unimplemented method 'getAccountByUsername'");
+        return accountRepository.getByUsernameIgnoreCase(username);
     }
 }
