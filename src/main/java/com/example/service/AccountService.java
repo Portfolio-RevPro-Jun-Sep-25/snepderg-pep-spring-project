32d184034b2885ce2af131866c36c77600e2f648
banoargeek <thatguymajor@protonmail.com>
2025-08-04 15:49:53 -0600

    Write Service Boilerplate

--- src/main/java/com/example/service/AccountService.java
+++ src/main/java/com/example/service/AccountService.java
@@ -1,4 +1,30 @@
 package com.example.service;
 
+import com.example.entity.Account;
+import com.example.repository.AccountRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import javax.transaction.Transactional;
+import java.sql.Connection;
+import java.util.List;
+
+/**
+ * This service class demonstrates the use of the AccountRepository.
+ */
+@Service
 public class AccountService {
+    AccountRepository accountRepository;
+    @Autowired // Constructor injection
+    public AccountService(AccountRepository accountRepository) {
+        this.accountRepository = accountRepository;
+    }
+    public Account registerAccount(Account account) {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'registerAccount'");
+    }
+    public Account getAccountByUsername(String username) {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'getAccountByUsername'");
+    }
 }
--- src/main/java/com/example/service/MessageService.java
+++ src/main/java/com/example/service/MessageService.java
@@ -1,4 +1,48 @@
 package com.example.service;
 
+import com.example.entity.Message;
+import com.example.repository.MessageRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import javax.transaction.Transactional;
+import javax.validation.Valid;
+
+import java.sql.Connection;
+import java.util.List;
+
+/**
+ * This service class demonstrates the use of the MessageRepository.
+ */
+@Service
 public class MessageService {
+    MessageRepository MessageRepository;
+    @Autowired // Constructor injection
+    public MessageService(MessageRepository MessageRepository) {
+        this.MessageRepository = MessageRepository;
+    }
+    public List<Message> getAllMessages() {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'getAllMessages'");
+    }
+    public Message getMessageById(int message_id) {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'getMessageById'");
+    }
+    public List<Message> getAllMessagesByAccountId(int account_id) {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'getAllMessagesByAccountId'");
+    }
+    public Message postMessage(@Valid Message message) {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'postMessage'");
+    }
+    public Message updateMessageById(int message_id) {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'updateMessageById'");
+    }
+    public Message deleteMessageById(int message_id) {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'deleteMessageById'");
+    }
 }
