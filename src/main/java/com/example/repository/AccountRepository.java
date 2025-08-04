e5223930a74679dbc5da1c2c7a6d1727b1c2c9c5
banoargeek <thatguymajor@protonmail.com>
2025-08-04 15:56:02 -0600

    Add Repository Annotation

--- src/main/java/com/example/repository/AccountRepository.java
+++ src/main/java/com/example/repository/AccountRepository.java
@@ -1,14 +1,17 @@
 package com.example.repository;
 
 import com.example.entity.Account;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Modifying;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
+import org.springframework.stereotype.Repository;
 
 import java.util.List;
 
 // TODO: Determine what information should be present for JpaRepository extension.
 //public interface PetRepository extends JpaRepository<T, ...>
+@Repository
 public interface AccountRepository {
 }
