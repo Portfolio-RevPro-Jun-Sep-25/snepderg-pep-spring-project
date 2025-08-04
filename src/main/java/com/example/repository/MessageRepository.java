baf39767086d6712faa72a3f6f98b9e39222b98f
banoargeek <thatguymajor@protonmail.com>
2025-08-04 15:49:39 -0600

    Write Repository Boilerplate

--- src/main/java/com/example/repository/MessageRepository.java
+++ src/main/java/com/example/repository/MessageRepository.java
@@ -1,4 +1,14 @@
 package com.example.repository;
 
+import com.example.entity.Message;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+
+import java.util.List;
+
+// TODO: Determine what information should be present for JpaRepository extension.
+//public interface PetRepository extends JpaRepository<T, ...>
 public interface MessageRepository {
 }
