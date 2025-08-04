7c6bcb8aafea9e34ca026bf20fc40c0d7bc57bb0
banoargeek <thatguymajor@protonmail.com>
2025-08-04 17:08:02 -0600

    Update Account Repository to use Spring Data JPA

--- src/main/java/com/example/repository/AccountRepository.java
+++ src/main/java/com/example/repository/AccountRepository.java
@@ -2,13 +2,9 @@ package com.example.repository;
 
 import com.example.entity.Account;
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.jpa.repository.Modifying;
-import org.springframework.data.jpa.repository.Query;
-import org.springframework.data.repository.query.Param;
 
-import java.util.List;
-
-// TODO: Determine what information should be present for JpaRepository extension.
-//public interface PetRepository extends JpaRepository<T, ...>
-public interface AccountRepository {
+// Spring Data JPA will automatically implement methods based on method naming conventions.
+public interface AccountRepository extends JpaRepository<Account, Integer> {
+    boolean existsByUsernameIgnoreCase(String username);
+    Account getByUsernameIgnoreCase(String username);
 }
