7788c679dd51f0dfbab8f2c8544b0777aa93505d
Kyle Bochman <thatguymajor@protonmail.com>
2025-08-05 01:15:44 +0000

    Remove unneeded dependencies

--- src/main/java/com/example/service/AccountService.java
+++ src/main/java/com/example/service/AccountService.java
@@ -5,10 +5,6 @@ import com.example.repository.AccountRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
-import javax.transaction.Transactional;
-import java.sql.Connection;
-import java.util.List;
-
 /**
  * This service class demonstrates the use of the AccountRepository.
  */
