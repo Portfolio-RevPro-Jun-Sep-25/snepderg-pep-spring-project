04dd3389fd1209586f100e9d832a5780dc98fbf2
banoargeek <thatguymajor@protonmail.com>
2025-08-04 18:16:32 -0600

    Fix Social Media Controller
    Added constructor injection, removed unused dependencies.

--- src/main/java/com/example/controller/SocialMediaController.java
+++ src/main/java/com/example/controller/SocialMediaController.java
@@ -6,13 +6,12 @@ import com.example.service.AccountService;
 import com.example.entity.Message;
 import com.example.service.MessageService;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
-import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import javax.validation.Valid;
-import javax.websocket.server.PathParam;
 
 import java.util.List;
 
@@ -28,6 +27,12 @@ import java.util.List;
 public class SocialMediaController {
     AccountService accountService;
     MessageService messageService;
+
+    @Autowired
+    public SocialMediaController(AccountService accountService, MessageService messageService) {
+        this.accountService = accountService;
+        this.messageService = messageService;
+    }
 
     // GET
     @GetMapping("/messages")
