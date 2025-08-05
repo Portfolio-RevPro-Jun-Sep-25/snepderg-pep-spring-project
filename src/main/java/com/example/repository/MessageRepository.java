3c13d2c743a8082c26385fcc3bf8ad1cb69f5845
Kyle Bochman <thatguymajor@protonmail.com>
2025-08-05 00:55:46 +0000

    Fix Message Repository

--- src/main/java/com/example/repository/MessageRepository.java
+++ src/main/java/com/example/repository/MessageRepository.java
@@ -4,6 +4,7 @@ import com.example.entity.Message;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Modifying;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
 
 import javax.transaction.Transactional;
