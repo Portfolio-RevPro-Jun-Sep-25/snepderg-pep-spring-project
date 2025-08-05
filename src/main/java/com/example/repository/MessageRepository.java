1d5ae5bde39552ff76a53b068a55df7a6fb20d4e
banoargeek <thatguymajor@protonmail.com>
2025-08-04 18:00:03 -0600

    Fix Message Repository findAllbyAccountId parameter

--- src/main/java/com/example/repository/MessageRepository.java
+++ src/main/java/com/example/repository/MessageRepository.java
@@ -7,5 +7,6 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import java.util.List;
 
 public interface MessageRepository extends JpaRepository<Message, Integer> {
-    List<Message> findAllByAccountId(int accountId);
+    // postedBy refers to the Account's accountId
+    List<Message> findAllByAccountId(int postedBy);
 }
